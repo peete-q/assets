831dca61fb27b6c080c3fba06ed2138c1124a9ae
peete-q <peete.q@gmail.com>
2014-01-23 15:36:37 +0800

    accelerate ok

---
+++ lua/Strip.lua
@@ -0,0 +1,18 @@
+
+
+local Strip = {}
+
+function Strip.new(deck, u1, v1, u2, v2)
+end
+
+function Strip:start(x1, y1, x2, y2)
+end
+
+function Strip:extend(x, y)
+end
+
+function Strip:stop()
+end
+
+function Strip:destroy()
+end
